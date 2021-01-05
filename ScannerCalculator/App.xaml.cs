253ff45cd576d29ea3716ab6748443542574ced6
Mateusz <mateusz.hamera@evatronix.com>
2021-01-05 21:47:14 +0100

    Add project files.

---
+++ ScannerCalculator/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using ScannerCalculator.Model;
+using ScannerCalculator.View;
+using ScannerCalculator.ViewModel;
+using System.Windows;
+
+namespace ScannerCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+
+            //Register Models
+            containerBuilder.RegisterType<Scanner>();
+            containerBuilder.RegisterType<ScannerInformations>();
+            containerBuilder.RegisterType<ScannerInformationsCalculator>();
+            containerBuilder.RegisterType<ScannerSuggestions>();
+            containerBuilder.RegisterType<ScannerSuggestionsCalculator>();
+
+            //Register ViewModels
+            containerBuilder.RegisterType<ScannerDataViewModel>();
+            containerBuilder.RegisterType<ScannerInformationsViewModel>();
+            containerBuilder.RegisterType<ScannerSuggestionsViewModel>();
+
+            //Register Views
+            containerBuilder.RegisterType<ScannerDataView>();
+            containerBuilder.RegisterType<ScannerInformationsView>();
+            containerBuilder.RegisterType<ScannerSuggestionsView>();
+
+            containerBuilder.RegisterType<MainWindow>();
+            containerBuilder.RegisterType<MainWindowViewModel>();
+
+            var container = containerBuilder.Build();
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ ScannerCalculator/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ScannerCalculator.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerCalculator
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel
+        (
+            ScannerDataViewModel scannerDataViewModel,
+            ScannerInformationsViewModel scannerInformationsViewModel,
+            ScannerSuggestionsViewModel scannerSuggestionsViewModel
+        )
+        {
+            ScannerData = scannerDataViewModel;
+            ScannerInformations = scannerInformationsViewModel;
+            ScannerSuggestions = scannerSuggestionsViewModel;
+        }
+
+        public ScannerDataViewModel ScannerData { get;}
+        public ScannerInformationsViewModel ScannerInformations { get; }
+        public ScannerSuggestionsViewModel  ScannerSuggestions{ get; }
+    }
+}
