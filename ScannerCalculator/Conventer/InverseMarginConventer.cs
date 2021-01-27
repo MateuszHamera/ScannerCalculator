34ebcb1129c9b6ad419d0400965342c53653d39d
Mateusz <mateusz.hamera@evatronix.com>
2021-01-27 15:09:17 +0100

    Added functionality  - can added ROI in Calibration Panel - ROI send to camera and finde correct exposure.

---
+++ ScannerCalculator/Conventer/InverseMarginConventer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ScannerCalculator.Conventer
+{
+    public class InverseMarginConventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int min = 150;
+            int max = 600;
+            int v = ((int)value - min) * 100;
+            double difference = max - min;
+            double a = difference / 100;
+            double percent = (double)v / difference;
+            double result = -a * percent + 600;
+
+            return new Thickness(result - 150, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScannerCalculator/Conventer/MarginConventer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ScannerCalculator.Conventer
+{
+    public class MarginConventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Thickness(System.Convert.ToDouble(value) - 150, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
